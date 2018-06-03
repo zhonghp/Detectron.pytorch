1e481fa5c1babf70689aec72281fd6b7e6c5fd3f
zhonghp <zhonghp@foxmail.com>
2018-06-03 22:32:02 +0800

    add half_head dataset config

--- lib/datasets/dataset_catalog.py
+++ lib/datasets/dataset_catalog.py
@@ -216,5 +216,11 @@ DATASETS = {
             _DATA_DIR + '/VOC2012/annotations/voc_2012_trainval.json',
         DEVKIT_DIR:
             _DATA_DIR + '/VOC2012/VOCdevkit2012'
+    },
+    'half_head_trainval': {
+        IM_DIR:
+            _DATA_DIR + '/half_head/JPEGImages',
+        ANN_FN:
+            _DATA_DIR + '/half_head/half_head_trainval.json'
     }
 }
