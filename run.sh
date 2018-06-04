c1629cd240a10c04f6a83ab28a19d98431a06633
zhonghp <zhonghp@foxmail.com>
2018-06-04 10:19:30 +0800

    add yaml config file for business

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/baselines/e2e_faster_rcnn_R-50-FPN_1x.yaml
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/e2e_faster_rcnn_R-50-FPN_1x.yaml
