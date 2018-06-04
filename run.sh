15829632096ef4c524a2ae0447efbf6714ddd5f4
zhonghp <zhonghp@foxmail.com>
2018-06-04 12:43:34 +0800

    add faster rcnn config

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/e2e_faster_rcnn_R-50-FPN_1x.yaml
+# CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/e2e_faster_rcnn_R-50-FPN_1x.yaml --nw 0
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/e2e_faster_rcnn_R-50-C4_1x.yaml --nw 0
