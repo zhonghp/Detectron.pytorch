0fad80323f5c8522581f1a9456f08d5508c88c89
zhonghp <zhonghp@foxmail.com>
2018-06-04 12:49:01 +0800

    add half_head in business configs

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/e2e_faster_rcnn_R-50-FPN_1x.yaml --nw 0
-CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/e2e_faster_rcnn_R-50-C4_1x.yaml --nw 0
+# CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/half_head/e2e_faster_rcnn_R-50-FPN_1x.yaml --nw 0
+CUDA_VISIBLE_DEVICES=0 python tools/train_net_step.py --dataset half_head --cfg configs/business/half_head/e2e_faster_rcnn_R-50-C4_1x.yaml --nw 0
